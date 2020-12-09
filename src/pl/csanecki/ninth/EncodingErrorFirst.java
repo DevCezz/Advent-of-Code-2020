d2cd21e512651aadf7e20456540b980b0e916644
Cezary Sanecki <devcezz@gmail.com>
2020-12-09 11:27:45 +0100

    Finished ninth task

---
+++ src/pl/csanecki/ninth/EncodingErrorFirst.java
@@ -0,0 +1,56 @@
+package pl.csanecki.ninth;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EncodingErrorFirst {
+
+    private static final int PREAMBLE = 25;
+
+    public static List<String> readData(String path) {
+        try {
+            return Files.readAllLines(Path.of(path));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Not found file: " + path);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Long> numbers = readData("data/ninth/task.txt").stream()
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+
+        int preamble = PREAMBLE;
+
+        long answer = 0;
+        for (int i = preamble; i < numbers.size(); i++) {
+            Long numberToCheck = numbers.get(i);
+
+            if (cannotBeGetFromNumbers(numberToCheck, numbers.subList(preamble - PREAMBLE, preamble))) {
+                answer = numberToCheck;
+                break;
+            }
+
+            preamble++;
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean cannotBeGetFromNumbers(Long numberToCheck, List<Long> subList) {
+        for (int i = 0; i < subList.size(); i++) {
+            for (int j = i + 1; j < subList.size(); j++) {
+                if (subList.get(i) + subList.get(j) == numberToCheck) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
